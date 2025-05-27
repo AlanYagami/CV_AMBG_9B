0d7e4ea4c89850bc3576813f7baec29bc05fcd95
AlanYagami <20223tn046@utez.edu.mx>
2025-05-27 15:51:00 -0600

    [FIRST COMMIT] subida inicial de proyecto

---
+++ src/main/java/mx/edu/utez/cvambg9b/Cvambg9bApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvambg9b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvambg9bApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvambg9bApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mx/edu/utez/cvambg9b/Cvambg9bApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.cvambg9b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cvambg9bApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
